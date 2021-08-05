4c757be091bccdb290fb4412d8fd134c78d77567
valleykid <valleykiddy@gmail.com>
2021-08-05 14:53:10 +0800

    fix: delete lib for gitignore

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
+import { ISandbox, MidwareSystem, RealMicroApp, NextFn } from '@satumjs/types';
 import { importEntry } from 'import-html-entry';
 import { createSandboxContainer, css } from './sandbox';
-import { MidwareSystem, RealMicroApp, NextFn } from '@satumjs/types';
 
-class QiankunSandbox {}
+class QiankunSandbox implements ISandbox {}
 
 export default function qiankunSandboxMidware(system: MidwareSystem, _: RealMicroApp[], next: NextFn) {
   system.set('sandbox', QiankunSandbox);
